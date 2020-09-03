9f3ef83300a3ed40257493e5b4d1dcf0e9f88183
Kristy Keller <keller.kristy@outlook.com>
2020-09-03 01:42:38 -0400

    number of character limits added

--- Develop/script.js
+++ Develop/script.js
@@ -21,20 +21,34 @@ function generatePassword() {
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "_", "-"]
+
 // Get user input through prompts and confirms 
-prompt("How many characters would you like your password to contain?");
 
-"Password length must be at least 8 characters"
-"Password length must be less than 129 characters"
+// First prompt asks number of characters 
+// If user doesn't select a number of characters between 8-158, they will be prompted with an error
+// If user does select a number within the criteria, the are directed to confirm numeric characters
+var numberOfCharacters = prompt("How many characters would you like your password to contain?");
+if (numberOfCharacters > 8) {
+alert("Click OK to confirm including numeric characters");
+} else if (numberOfCharacters < 8) {
+  alert("Password length must be at least 8 characters")
+} if (numberOfCharacters < 128) {
+  alert("Click OK to confirm including numeric characters");
+} else if (numberOfCharacters > 128) {
+  alert("Password length must be less than 129 characters")
+}
 
-"Click OK to confirm including special characters"
 "Click OK to confirm including numeric characters"
+"Click OK to confirm including special characters"
 "Click OK to confirm including lowercase characters"
 "Click OK to confirm including uppercase characters"
 // Get user to make their options meet the criteria
 
 // Error check--using alerts, make use user options meet the criteria 
+
 "Must select at least one character type"
+
+
 // Generate a password 
 
 return password;
