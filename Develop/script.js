084198ec4cda44fb9a92072cf26df02f099905e7
Kristy Keller <keller.kristy@outlook.com>
2020-09-03 02:32:23 -0400

    psuedo code added

--- Develop/script.js
+++ Develop/script.js
@@ -25,17 +25,23 @@ function generatePassword() {
 // Get user input through prompts and confirms 
 
 // First prompt asks number of characters 
-// If user doesn't select a number of characters between 8-158, they will be prompted with an error
-// If user does select a number within the criteria, the are directed to confirm numeric characters
+// Using while loop; If user doesn't select a number of characters between 8-158, they will be prompted with an error
+// Using while loop; If user does select a number within the criteria, the are directed to confirm numeric characters
 var numberOfCharacters=parseInt(prompt("How many characters do you want your password to be?"));
     while(numberOfCharacters < 8 || numberOfCharacters > 128 || typeof(numberOfCharacters) != "number" || numberOfCharacters === NaN || numberOfCharacters === null) {
         alert("Please choose a number that is more than 8 and less than 128 characters.");
-        numberOfCharacters=parseInt(prompt("How many characters do you want the password to be?"));
+        numberOfCharacters=parseInt(prompt("How many characters do you want your password to be?"));
     } 
+
 // Need user to select correct criteria next
-"Click OK to confirm including numeric characters"
-"Click OK to confirm including special characters"
+// Use concat array? Which loop? 
+var lowerCase;
+for (lowerCase
+
 "Click OK to confirm including lowercase characters"
+"Click OK to confirm including special characters"
+
+"Click OK to confirm including numeric characters"
 "Click OK to confirm including uppercase characters"
 
 
